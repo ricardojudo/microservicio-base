a023dc0aef620688b02bbe230e72284ea1f8e787
gusdelact <gusdelact@192.168.1.72>
2018-05-23 13:46:42 -0500

    se corrigio la anotacion a RestController

--- src/main/java/com/gfi/microservicios/controller/MicroServicioController.java
+++ src/main/java/com/gfi/microservicios/controller/MicroServicioController.java
@@ -1,10 +1,11 @@
 package com.gfi.microservicios.controller;
 
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping(value = "/v1/api/microservicio")
 
 public class MicroServicioController {
