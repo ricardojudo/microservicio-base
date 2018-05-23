c7c9c818db5fe7b3c2f1369711b85babbfe21882
gusdelact <gusdelact@192.168.1.72>
2018-05-23 13:43:11 -0500

    Correccion al controlador

--- src/main/java/com/gfi/microservicios/controller/MicroServicioController.java
+++ src/main/java/com/gfi/microservicios/controller/MicroServicioController.java
@@ -2,13 +2,14 @@ package com.gfi.microservicios.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping(value = "/v1/api/microservicio")
 
 public class MicroServicioController {
 
-	@RequestMapping(value = "/metodo")
+	@RequestMapping(value="/",method = RequestMethod.GET)
 	public String metodo() {
 		return "una cadena";
 	}
