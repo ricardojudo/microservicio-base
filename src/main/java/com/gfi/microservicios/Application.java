01bac74fdc5db5257bf1c69fd40355706417e34d
gusdelact <gusdelact@192.168.1.72>
2018-05-24 00:57:03 -0500

    Registro del microservicio con SpringCloud Eureka. Se agregaron dependencias en pom.xml . La dependencia de Spring Cloud es la version Camden.SR7

--- src/main/java/com/gfi/microservicios/Application.java
+++ src/main/java/com/gfi/microservicios/Application.java
@@ -2,10 +2,11 @@ package com.gfi.microservicios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Application {
-
 	public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
