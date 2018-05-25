25632608388c76f8d533bb6b99055d14c07e6370
Gustavo De la Cruz Tovar <gusdelact@Gustavo-De-la-Cruz-Tovars-MacBook.local>
2018-05-25 13:27:14 -0500

    se modifico MicroServicioController para invocar un microservicio dummy. Se utiliza el Cliente de Descubrimiento de Eureka de Spring Cloud

--- src/main/java/com/gfi/microservicios/controller/MicroServicioController.java
+++ src/main/java/com/gfi/microservicios/controller/MicroServicioController.java
@@ -1,17 +1,38 @@
 package com.gfi.microservicios.controller;
 
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.gfi.microservicios.client.DummyDiscoveryClient;
 
 @RestController
 @RequestMapping(value = "/microservicio")
 
 public class MicroServicioController {
+	
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
+	@Autowired
+	DummyDiscoveryClient dummyDiscoveryClient;
+	
+	
 	@RequestMapping(value="",method = RequestMethod.GET)
 	public String metodo() {
 		return "una cadena de un microservicio registrado con Eureka";
+	}
+	
+	@RequestMapping(value="/dummy",method = RequestMethod.GET)
+	public String metodoDummy() {
+		String resultado= "hola dummy";
+		resultado=dummyDiscoveryClient.getDummy();
+		if (resultado==null) resultado= "hola dummy";
+		logger.info(resultado);
+		return resultado;
 	}
 }
